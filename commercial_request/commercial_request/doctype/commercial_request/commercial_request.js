7d3a101afdb33fd075984434f5cfe0fa4f44f776
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-03-04 17:51:16 +0500

    Commercial Request Doctype

---
+++ commercial_request/commercial_request/doctype/commercial_request/commercial_request.js
@@ -0,0 +1,31 @@
+// Copyright (c) 2024, abdul basit ali and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Commercial Request', {
+
+	sales_invoice_number:function(frm){
+		if(frm.doc.sales_invoice_number){
+			frm.call({
+				doc:frm.doc,
+				method:"get_sales_invoice_items",
+				freeze: true,
+				callback: function(r) {
+
+				}
+			})
+		}
+
+	},
+
+	setup:function(frm){
+		frm.set_query("sales_invoice_number",function(doc){
+			return{
+				filters : {
+					customer:doc.customer
+				}
+			}
+
+		})
+
+	}
+});
---
+++ commercial_request/commercial_request/doctype/commercial_request/test_commercial_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, abdul basit ali and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCommercialRequest(FrappeTestCase):
+	pass
