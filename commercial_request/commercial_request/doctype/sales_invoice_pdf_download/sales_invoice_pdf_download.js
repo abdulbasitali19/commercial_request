03ab331d79ba825bc9a2ef9b6b0ac4e574af9fc1
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-04-06 02:47:59 +0000

    downlaod multiple sales invoices completed

--- commercial_request/commercial_request/doctype/sales_invoice_pdf_download/sales_invoice_pdf_download.js
+++ commercial_request/commercial_request/doctype/sales_invoice_pdf_download/sales_invoice_pdf_download.js
@@ -3,29 +3,55 @@
 
 frappe.ui.form.on('Sales Invoice Pdf Download', {
 	onload:function(frm){
-		debugger;
 		frm.doc.sales_invoice_list = []
 	},
 
 	refresh: function(frm) {
-		var sales_invoice_list = frm.doc.sales_invoice_list
-	if(sales_invoice_list.length >= 1){
-		frm.add_custom_button(__("Download Sales Invoice PDF"), function () {
-			frm.call({
-				doc: frm.doc,
-				method: "download_sales_invoice_pdf",
-				freeze: true,
-				callback: function (r) {
-					r = r.message
-					console.log(r)
-				}
-			})
-
-
-		});
-	}	
+		var sales_invoice_list = frm.doc.sales_invoice_list.slice(); 
+		if (sales_invoice_list.length >= 1) {
+			frm.add_custom_button(__("Download Sales Invoice PDF"), function() {
+				downloadInvoice(sales_invoice_list, function() {
+					frm.set_value('sales_invoice_list', sales_invoice_list);
+					frm.refresh_field('sales_invoice_list'); 
+					frappe.msgprint(__("All selected sales invoices have been downloaded."));
+				});
+			});
+		}
+	
+		function downloadInvoice(list, onComplete) {
+			if (list.length === 0) {
+				onComplete(); 
+				return;
+			}
+	
+			
+			var currentInvoice = list.shift(); 
+	
+			var downloadUrl = frappe.urllib.get_full_url("/api/method/commercial_request.commercial_request.doctype.sales_invoice_pdf_download.sales_invoice_pdf_download.pdf_download_custom?" +
+				new URLSearchParams({
+					name: currentInvoice.sales_invoice,
+					print_format: frm.doc.print_format || "Standard",
+					language: frappe.boot.lang,
+					letterhead: frm.doc.letter_head || "",
+				}).toString()
+			);
+	
+			
+			var link = document.createElement('a');
+			link.href = downloadUrl;
+			link.download = currentInvoice.sales_invoice + '.pdf'; 
+			document.body.appendChild(link); 
+			link.click(); 
+			document.body.removeChild(link);
+	
+			setTimeout(function() {
+				downloadInvoice(list, onComplete); 
+			}, 1000); 
+		}
 	},
-
+	
+	
+	
 	setup:function(frm){
 		frm.set_query("print_format",function(){
 			return {
--- commercial_request/commercial_request/doctype/sales_invoice_pdf_download/sales_invoice_pdf_download.py
+++ commercial_request/commercial_request/doctype/sales_invoice_pdf_download/sales_invoice_pdf_download.py
@@ -5,7 +5,7 @@ import frappe
 import requests
 from frappe.model.document import Document
 from urllib.parse import urlencode, quote
-# from frappe.utils.weasyprint import download_pdf, get_html
+# from frappe.utils.weasyprint import download_pdf
 # from frappe.utils.pdf import get_pdf
 from frappe.utils.print_format import download_pdf
 from frappe.utils import get_url
@@ -33,72 +33,12 @@ class SalesInvoicePdfDownload(Document):
                         "sales_invoice": sales_invoice.get("name")
                     })
 
-    @frappe.whitelist(allow_guest = True)
-    def download_sales_invoice_pdf(self):
-        if len(self.sales_invoice_list) > 0:
-             for invoice in self.sales_invoice_list:
-                doc = frappe.get_doc("Sales Invoice", invoice.get("sales_invoice"))
-                download_pdf("Sales Invoice", invoice.get("sales_invoice"), self.print_format, doc =doc,language=None,letterhead=None)
+
+@frappe.whitelist(allow_guest = True)
+def pdf_download_custom(name: str,print_format: Optional[str] = None, language: Optional[str] = None,letterhead: Optional[str] = None,):
+        doc = frappe.get_doc("Sales Invoice", name)
+        download_pdf(doc.doctype, doc.name, print_format,doc=doc, language = language,  letterhead=letterhead or None,)
+  
 
 
 
-
-# @frappe.whitelist()
-# def get_pdf(
-# 	name: str,
-# 	supplier: str,
-# 	print_format: Optional[str] = None,
-# 	language: Optional[str] = None,
-# 	letterhead: Optional[str] = None,
-# ):
-# 	doc = frappe.get_doc("Request for Quotation", name)
-# 	if supplier:
-# 		doc.update_supplier_part_no(supplier)
-
-# 	# permissions get checked in `download_pdf`
-# 	download_pdf(
-# 		doc.doctype,
-# 		doc.name,
-# 		print_format,
-# 		doc=doc,
-# 		language=language,
-# 		letterhead=letterhead or None,
-# 	)
-
-
-
-
-
-#     @frappe.whitelist(allow_guest = True)		
-#     def download_sales_invoice_pdf(self):
-#         if len(self.sales_invoice_list) > 0:
-#             for i in self.sales_invoice_list:
-#                 download_pdf(i.get("sales_invoice"),self.print_format)
-
-
-# def download_pdf(name,format):
-#     # base_url = "https://pvhmiddleeast.codeplus.solutions/api/method/frappe.utils.print_format.download_pdf"
-#     base_url = "http://mysite.localhost:8000/api/method/frappe.utils.print_format.download_pdf"
-#     params = {
-#         'doctype': 'Sales Invoice',
-#         'name': name,  # Example name
-#         'format': format,
-#         'no_letterhead': '1',
-#         'letterhead': 'No Letterhead',
-#         'settings': '%',  # The settings parameter you provided, note that it's "%"
-#         '_lang': 'en'
-#     }
-#     url = f"{base_url}?{urlencode(params)}"
-#     generated_secret = frappe.utils.password.get_decrypted_password("User", "Administrator", fieldname='api_secret')
-#     api_key = frappe.db.get_value("User", "Administrator", "api_key")
-#     header = {"Authorization": "token {}:{}".format(api_key, generated_secret)}
-#     # doc = get_pdf(url)
-#     res = requests.get(url, headers=header,verify=False)
-#     if res.status_code == 200:
-#         frappe.msgprint(res)
-#         with open('file.pdf', 'wb') as f:
-#             f.write(res.content)
-#         return f
-#     else:
-#         print("Failed to download PDF:", res.status_code)
-#         return None
